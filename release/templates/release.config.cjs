cdfe4c953f6ca01a36861d25500cac5c3f2b6e64
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2024-02-27 23:57:59 +0100

    fix: update release config

--- release/templates/release.config.cjs
+++ release/templates/release.config.cjs
@@ -1,11 +1,27 @@
-const path = require('path');
 const pkg = require('./package.json');
 
-const root = path.join(__dirname, '..', '..', '..');
-const releaseConfig = path.join(root, 'release', 'release.config.cjs');
 const pkgTag = pkg.name.replace('@', '').replace('/', '-');
 
 module.exports = {
-  extends: releaseConfig,
-  tagFormat: `${pkgTag}-v\${version}`
+  extends: "semantic-release-monorepo",
+  tagFormat: `${pkgTag}-v\${version}`,
+  dryRun: true,
+  "branches": ["monorepo"],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["docs/CHANGELOG.md", "package.json"]
+      }
+    ]
+  ]
 }

67b6d3acfbf5f323b514595005f67d31cb6c91f9
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2024-02-28 00:03:57 +0100

    fix: update root dir

--- release/getChangedPackages.js
+++ release/getChangedPackages.js
@@ -3,12 +3,12 @@ const { join, resolve } = require('path');
 const { runProcess } = require('./runProcess');
 
 const getDirectories = (srcPath, baseDir) => {
-  const dir = resolve(srcPath, baseDir);
+  const dir = join(srcPath, baseDir);
   return fs.readdirSync(dir).map(file => join(dir, file)).filter((filePath) => fs.statSync(filePath).isDirectory())
 }
 //returns the abs path of all packages
 function getPackagesPaths() {
-  const root = '../packages';
+  const root = join(__dirname, '..', 'packages');
   const groups = ['blocks', 'scripts'].map((p) => getDirectories(root, p));
   return groups.flat();
 }
